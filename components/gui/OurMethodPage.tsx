0edff242ba1ce0ac035d47f6fa3fba265bb13d29
l0x539 <nogues322@gmail.com>
2023-08-03 17:56:54 +0100

    [Fix] remove cursor events for test tabs.

--- components/gui/OurMethodPage.tsx
+++ components/gui/OurMethodPage.tsx
@@ -5,27 +5,27 @@ const TABS = [
   {
     label: 'research',
     title: 'RESEARCH & DISCOVERY',
-    content: <div className="absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>RESEARCH</span></div>
+    content: <div className="pointer-events-none absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>RESEARCH</span></div>
   },
   {
     label: 'strategy',
     title: 'STRATEGY & DIRECTION',
-    content: <div className="absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>STRATEGY</span></div>
+    content: <div className="pointer-events-none absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>STRATEGY</span></div>
   },
   {
     label: 'design',
     title: 'DESIGN & DEVELOPMENT',
-    content: <div className="absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>DESIGN</span></div>
+    content: <div className="pointer-events-none absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>DESIGN</span></div>
   },
   {
     label: 'finesse',
     title: 'FINESSE & LAUNCH',
-    content: <div className="absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>FINESSE</span></div>
+    content: <div className="pointer-events-none absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>FINESSE</span></div>
   },
   {
     label: 'support',
     title: 'SUPPORT & MANAGEMENT',
-    content: <div className="absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>SUPPORT</span></div>
+    content: <div className="pointer-events-none absolute flex justify-center items-center w-screen h-screen text-3xl top-0 left-0 text-white"><span>SUPPORT</span></div>
   },
 ];
 
