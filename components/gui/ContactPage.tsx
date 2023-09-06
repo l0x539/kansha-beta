d0ae8466d9694ecc90c000e2c171d0e69f6a5980
l0x539 <nogues322@gmail.com>
2023-09-06 08:18:03 +0100

    [Fix] contact pixel perfect.

--- components/gui/ContactPage.tsx
+++ components/gui/ContactPage.tsx
@@ -29,11 +29,11 @@ const ContactPage = () => {
 
   return (
     <div className="absolute top-0 left-0 w-screen h-screen">
-      <div className="animate-fade-in pt-96 flex flex-col w-full h-full items-center">
-        <h1 className="text-9xl text-white mb-28">
+      <div className="animate-fade-in flex flex-col w-full h-full items-center">
+        <h1 className="absolute top-[40%] text-9xl text-white mb-28">
           Let{'’'}s talk
         </h1>
-        <Link href={'/contact/form' + (searchParams.get('demo') && COMING_SOON ? ('?' + createQueryString('demo', `${searchParams.get('demo')}`)) : '')} className="absolute bottom-48 group w-24 h-24 hover:cursor-pointer select-none">
+        <Link href={'/contact/form' + (searchParams.get('demo') && COMING_SOON ? ('?' + createQueryString('demo', `${searchParams.get('demo')}`)) : '')} className="absolute bottom-[20%] group w-24 h-24 hover:cursor-pointer select-none">
           <div  className={`absolute left-0 top-0 w-24 h-24 flex justify-center items-center rounded-full border group-hover:bg-[#5E5E5E] group-hover:border-transparent mix-blend-difference shadow-[0px_2.2926828861236572px_2.2926828861236572px_0px_rgba(0,0,0,0.25)_inset] backdrop-opacity-70`}></div>
           <div className={`absolute top-0 w-24 h-24 flex justify-center items-center group-hover:text-white text-base font-gothic font-light`}>
             <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
