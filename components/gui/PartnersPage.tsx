c60ac82fb3d65f71ce6bba3eeb42b1da59f00ee2
l0x539 <nogues322@gmail.com>
2023-09-07 05:58:28 +0100

    [Add] partners lapalma link.

--- components/gui/PartnersPage.tsx
+++ components/gui/PartnersPage.tsx
@@ -45,7 +45,7 @@ const PartnersPage = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec interdum erat. Nulla facilisi. Fusce tincidunt ante in velit laoreet, ut pellentesque nunc eleifend. Cras et ex eget justo posuere malesuada. Curabitur at cursus justo.
         <br/><br/>Sed auctor justo sed massa convallis, in rhoncus odio gravida. Nam vitae diam vitae felis tincidunt tristique.
       </Partner>
-      <Partner title="La Palma" className="hover:bg-la-palma" more={"/portfolio/lazo" + (searchParams.get('demo') && COMING_SOON ? ('?' + createQueryString('demo', `${searchParams.get('demo')}`)) : '')}>
+      <Partner title="La Palma" className="hover:bg-la-palma" more={"/portfolio/lapalma" + (searchParams.get('demo') && COMING_SOON ? ('?' + createQueryString('demo', `${searchParams.get('demo')}`)) : '')}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec interdum erat. Nulla facilisi. Fusce tincidunt ante in velit laoreet, ut pellentesque nunc eleifend. Cras et ex eget justo posuere malesuada. Curabitur at cursus justo.
         <br/><br/>Sed auctor justo sed massa convallis, in rhoncus odio gravida. Nam vitae diam vitae felis tincidunt tristique.
       </Partner>
